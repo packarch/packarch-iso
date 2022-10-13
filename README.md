f81da6179ae5c33a0a568d1c92f230f3c0f3cffb
Antidote1911 <antidote1911@gmail.com>
2022-10-13 23:45:17 +0200

    up

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# packarch-iso
+<p align="center">
+<a href="https://packarch.io"><img src="https://raw.githubusercontent.com/packarch-os/packarch-packages/main/packarch-artworks/files/logo/png/logo-circle/logo-circle-1.png" height="128" width="128" alt="Packarch"></a>
+</p>
+
+<p align="center">
+<a href="https://www.buymeacoffee.com/adi1090x"><img width="32px" src="https://raw.githubusercontent.com/adi1090x/files/master/other/1.png" alt="Buy Me A Coffee"></a>
+<a href="https://ko-fi.com/adi1090x"><img width="32px" src="https://raw.githubusercontent.com/adi1090x/files/master/other/2.png" alt="Donate for Packarch on ko-fi"></a>
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6VETHHYHXESRN"><img width="32px" src="https://raw.githubusercontent.com/adi1090x/files/master/other/3.png" alt="Donate for Packarch via Paypal"></a>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/Maintained%3F-Yes-green?style=flat-square">
+  <img src="https://img.shields.io/github/downloads/packarch-os/releases/total?label=downloads&logo=github&color=blue&style=flat-square">
+  <img src="https://img.shields.io/sourceforge/dt/packarch.svg?label=downloads&logo=sourceforge&color=teal&style=flat-square">
+  <img src="https://img.shields.io/github/stars/packarch-os/packarch?style=flat-square">
+  <img src="https://img.shields.io/github/issues/packarch-os/packarch?color=violet&style=flat-square">
+</p>
+
+<p align="center">
+Yet another minimal linux distribution, based on <a href="https://www.archlinux.org">Arch Linux</a>.
+</p>
+
+<p align="center">
+  <a href="https://packarch.io" target="_blank"><img alt="home" src="https://img.shields.io/badge/HOME-blue?style=flat-square"></a>
+  <a href="https://wiki.packarch.io" target="_blank"><img alt="wiki" src="https://img.shields.io/badge/WIKI-blue?style=flat-square"></a>
+  <a href="https://packarch.io/gallery" target="_blank"><img alt="screenshots" src="https://img.shields.io/badge/SCREENSHOTS-blue?style=flat-square"></a>
+  <a href="https://www.reddit.com/r/packarch" target="_blank"><img alt="reddit" src="https://img.shields.io/badge/REDDIT-blue?style=flat-square"></a>
+  <a href="https://discord.gg/3PzeJ5S7Pu" target="_blank"><img alt="discord" src="https://img.shields.io/badge/DISCORD-blue?style=flat-square"></a>
+  <a href="https://t.me/packarchos" target="_blank"><img alt="telegram" src="https://img.shields.io/badge/TELEGRAM-blue?style=flat-square"></a>
+  <a href="https://matrix.to/#/#packarch:matrix.org" target="_blank"><img alt="matrix" src="https://img.shields.io/badge/MATRIX-blue?style=flat-square"></a>
+</p>
+
+![img](https://raw.githubusercontent.com/packarch-os/core-packages/main/calamares-config/files/calamares/branding/packarch/welcome.png)
+
+---
+
+### From Packarch Wiki
+
+- [Things To Do After Installing Packarch](https://wiki.packarch.io/docs/install-packarch/post-install)
+- [Install Packarch With Calamares](https://wiki.packarch.io/docs/install-packarch/install-with-calamares)
+- [Install Packarch With ABIF](https://wiki.packarch.io/docs/install-packarch/install-with-abif)
+- [Create A Bootable USB With Packarch](https://wiki.packarch.io/docs/boot-iso/boot-with-usb)
+- [Boot Packarch ISO With GRUB2 Bootloader](https://wiki.packarch.io/docs/boot-iso/boot-with-grub)
+
+### Get The ISO
+
+You can download latest release of `ISO` (see [`releases`](https://github.com/packarch-os/releases)), Available options are...
+<p align="center">
+  <a href="https://github.com/packarch-os/releases/releases/download/v21.10/packarch-2021.10.05-x86_64.iso" target="_blank"><img alt="undefined" src="https://img.shields.io/badge/Download-Github-blue?style=for-the-badge&logo=github"></a>&nbsp;&nbsp;
+  <a href="https://sourceforge.net/projects/packarch/files/latest/download" target="_blank"><img alt="undefined" src="https://img.shields.io/badge/Download-Sourceforge-orange?style=for-the-badge&logo=sourceforge"></a>&nbsp;&nbsp;
+  <a href="https://github.com/packarch-os/releases/releases/download/v21.10/packarch-2021.10.05-x86_64.iso.torrent" target="_blank"><img alt="undefined" src="https://img.shields.io/badge/Download-Torrent-magenta?style=for-the-badge&logo=discogs"></a>
+</p>
+
+> Default `username` and `password` is **liveuser**.
+
+> Don't forget to hit the **`STAR`** if you love Packarch.
---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,156 @@
+#!/usr/bin/env bash
+
+## Script to perform several important tasks before `mkpackarchiso` create filesystem image.
+
+set -e -u
+
+## -------------------------------------------------------------- ##
+
+# fr_FR.UTF-8 locales
+sed -i 's/#\(fr_FR\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+# France, Paris timezone
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+
+## Fix Initrd Generation in Installed System
+cat > "/etc/mkinitcpio.d/linux.preset" <<- _EOF_
+	# mkinitcpio preset file for the 'linux' package
+
+	ALL_kver="/boot/vmlinuz-linux"
+	ALL_config="/etc/mkinitcpio.conf"
+
+	PRESETS=('default' 'fallback')
+
+	#default_config="/etc/mkinitcpio.conf"
+	default_image="/boot/initramfs-linux.img"
+	#default_options=""
+
+	#fallback_config="/etc/mkinitcpio.conf"
+	fallback_image="/boot/initramfs-linux-fallback.img"
+	fallback_options="-S autodetect"
+_EOF_
+
+## -------------------------------------------------------------- ##
+
+## Enable Parallel Downloads
+sed -i -e 's|#ParallelDownloads.*|ParallelDownloads = 6|g' /etc/pacman.conf
+sed -i -e '/#\[testing\]/Q' /etc/pacman.conf
+
+## Append packarch repository to pacman.conf
+cat >> "/etc/pacman.conf" <<- EOL
+	[packarch]
+	SigLevel = Optional TrustAll
+	Include = /etc/pacman.d/packarch-mirrorlist
+
+	#[testing]
+	#Include = /etc/pacman.d/mirrorlist
+
+	[core]
+	Include = /etc/pacman.d/mirrorlist
+
+	[extra]
+	Include = /etc/pacman.d/mirrorlist
+
+	#[community-testing]
+	#Include = /etc/pacman.d/mirrorlist
+
+	[community]
+	Include = /etc/pacman.d/mirrorlist
+
+	# If you want to run 32 bit applications on your x86_64 system,
+	# enable the multilib repositories as required here.
+
+	#[multilib-testing]
+	#Include = /etc/pacman.d/mirrorlist
+
+	[multilib]
+	Include = /etc/pacman.d/mirrorlist
+
+	# An example of a custom package repository.  See the pacman manpage for
+	# tips on creating your own repositories.
+	#[custom]
+	#SigLevel = Optional TrustAll
+	#Server = file:///home/custompkgs
+EOL
+
+## Copy Few Configs Into Root Dir
+rdir="/root/.config"
+sdir="/etc/skel"
+if [[ ! -d "$rdir" ]]; then
+	mkdir "$rdir"
+fi
+
+rconfig=(alacritty geany gtk-3.0 Kvantum leafpad libfm neofetch nvim pcmanfm qt5ct ranger Thunar xfce4)
+for cfg in "${rconfig[@]}"; do
+	if [[ -e "$sdir/.config/$cfg" ]]; then
+		cp -rf "$sdir"/.config/"$cfg" "$rdir"
+	fi
+done
+
+rcfg=('.local' '.oh-my-zsh' '.gtkrc-2.0' '.vim_runtime' '.vimrc' '.zshrc')
+for cfile in "${rcfg[@]}"; do
+	if [[ -e "$sdir/$cfile" ]]; then
+		cp -rf "$sdir"/"$cfile" /root
+	fi
+done
+
+## -------------------------------------------------------------- ##
+
+## Don't launch welcome app on installed system, launch Help instead
+sed -i -e '/## Welcome-App-Run-Once/Q' /etc/skel/.config/openbox/autostart
+cat >> "/etc/skel/.config/openbox/autostart" <<- EOL
+	## Help-App-Run-Once
+	help-and-tips &
+	sed -i -e '/## Help-App-Run-Once/Q' "\$HOME"/.config/openbox/autostart
+EOL
+
+## -------------------------------------------------------------- ##
+
+## Set `Qogirr` as default cursor theme
+sed -i -e 's|Inherits=.*|Inherits=Qogirr|g' /usr/share/icons/default/index.theme
+mkdir -p /etc/skel/.icons && cp -rf /usr/share/icons/default /etc/skel/.icons/default
+
+## Update xdg-user-dirs for bookmarks in thunar and pcmanfm
+runuser -l liveuser -c 'xdg-user-dirs-update'
+runuser -l liveuser -c 'xdg-user-dirs-gtk-update'
+xdg-user-dirs-update
+xdg-user-dirs-gtk-update
+
+## Delete stupid gnome backgrounds
+gndir='/usr/share/backgrounds/gnome'
+if [[ -d "$gndir" ]]; then
+	rm -rf "$gndir"
+fi
+
+## -------------------------------------------------------------- ##
+
+## Hide Unnecessary Apps
+adir="/usr/share/applications"
+apps=(avahi-discover.desktop bssh.desktop bvnc.desktop echomixer.desktop \
+	envy24control.desktop exo-preferred-applications.desktop feh.desktop \
+	hdajackretask.desktop hdspconf.desktop hdspmixer.desktop hwmixvolume.desktop lftp.desktop \
+	libfm-pref-apps.desktop lxshortcut.desktop lstopo.desktop \
+	networkmanager_dmenu.desktop nm-connection-editor.desktop pcmanfm-desktop-pref.desktop \
+	qv4l2.desktop qvidcap.desktop stoken-gui.desktop stoken-gui-small.desktop thunar-bulk-rename.desktop \
+	thunar-settings.desktop thunar-volman-settings.desktop yad-icon-browser.desktop)
+
+for app in "${apps[@]}"; do
+	if [[ -e "$adir/$app" ]]; then
+		sed -i '$s/$/\nNoDisplay=true/' "$adir/$app"
+	fi
+done
+
+## -------------------------------------------------------------- ##
+
+## Lightdm config (Greeter, Autologin)
+lightdm_config='/etc/lightdm/lightdm.conf'
+sed -i -e 's|#greeter-session=.*|greeter-session=lightdm-gtk-greeter|g' "$lightdm_config"
+sed -i -e 's|#autologin-user=.*|autologin-user=liveuser|g' "$lightdm_config"
+sed -i -e 's|#autologin-session=.*|autologin-session=openbox|g' "$lightdm_config"
+sed -i -e 's|#greeter-setup-script=.*|greeter-setup-script=/usr/bin/numlockx on|g' "$lightdm_config"
+groupadd -r autologin
+gpasswd -a liveuser autologin
+systemctl enable lightdm
+
+## -------------------------------------------------------------- ##
